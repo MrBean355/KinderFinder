326a1b090b83a133c58e21f1b170f8e179461efc
MrBean355 <mrbean355@gmail.com>
2014-06-26 10:39:07 +0200

    Fixed SQL script

--- DataBase/KinderfinderSqlDatabase.sql
+++ DataBase/KinderfinderSqlDatabase.sql
@@ -2,7 +2,16 @@ DROP Database KinderFinder
 GO
 
 CREATE Database KinderFinder
+GO 
+
+USE KinderFinder
 GO
+
+CREATE TABLE Restaurant
+(
+	Restaurant_ID int primary key,
+	Restaurant_Name varchar(50)
+) 
 
 CREATE TABLE ApplicationUsers
 (
@@ -12,26 +21,18 @@ CREATE TABLE ApplicationUsers
 	User_PhoneModel varchar(50),
 	User_Password varchar(50),
 	User_PhoneNumber int,
-	User_Restaurant_ID int references Restaurant(RestaurantID)
+	User_Restaurant_ID int references Restaurant(Restaurant_ID)
 ) 
 
 CREATE TABLE RF_ID_Tags
 (
 	RF_ID_Number int primary key,
 	RF_ID_User_ID int references ApplicationUsers(UserID)
-) 
-
-CREATE TABLE Restaurant
-(
-	Restaurant_ID int primary key,
-	Restaurant_Name varchar(50)
-) 
+)
 
 INSERT into Restaurant VALUES (00001, 'Spur')
 INSERT into Restaurant VALUES (00002, 'Steers')
 INSERT into Restaurant VALUES (00003, 'Nandos')
-
-
 
 INSERT into ApplicationUsers VALUES ('0705617', 'Justin','Timberlake', 'phone1', 'password', 1234567890, 00001)
 INSERT into ApplicationUsers VALUES ('0705631', 'Mariah', 'Carrey', 'phone1', 'password', 1234567890, 00002)
@@ -42,7 +43,6 @@ INSERT into ApplicationUsers VALUES ('0725609', 'Andrea', 'Bocelli', 'phone1', '
 INSERT into ApplicationUsers VALUES ('0745603', 'Skwatta', 'Camp', 'phone1', 'password', 1234567890, 00003)
 INSERT into ApplicationUsers VALUES ('0765607', 'BB','King', 'phone1', 'password', 1234567890, 00002)
 INSERT into ApplicationUsers VALUES ('0785601', 'Dolly' ,'Parton', 'phone1', 'password', 1234567890, 00003)
-
 
 INSERT into RF_ID_Tags VALUES (0000112, 0705617)
 INSERT into RF_ID_Tags VALUES (0000113, 0705631)
