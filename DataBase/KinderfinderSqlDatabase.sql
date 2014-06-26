30ddf75386d1639c2ccadc9243affa75f2f9433d
MrBean355 <mrbean355@gmail.com>
2014-06-26 18:04:55 +0200

    Fixed SQL script more

--- DataBase/KinderfinderSqlDatabase.sql
+++ DataBase/KinderfinderSqlDatabase.sql
@@ -1,3 +1,8 @@
+/*
+SELECT * FROM [Restaurant]
+SELECT * FROM [ApplicationUser]
+SELECT * FROM [Tag]
+*/
 DROP Database KinderFinder
 GO
 
@@ -7,48 +12,51 @@ GO
 USE KinderFinder
 GO
 
-CREATE TABLE Restaurant
-(
-	Restaurant_ID int primary key,
-	Restaurant_Name varchar(50)
+CREATE TABLE [Restaurant] (
+	[ID] int IDENTITY(1, 1),
+	[Name] varchar(50),
+
+	PRIMARY KEY([ID])
 ) 
 
-CREATE TABLE ApplicationUsers
-(
-	UserID int primary key,
-	User_Name varchar(50),
-	User_Surname varchar(50),
-	User_PhoneModel varchar(50),
-	User_Password varchar(50),
-	User_PhoneNumber int,
-	User_Restaurant_ID int references Restaurant(Restaurant_ID)
+CREATE TABLE [ApplicationUser] (
+	[ID] int IDENTITY(1, 1),
+	[Name] varchar(50),
+	[Surname] varchar(50),
+	[Password] varchar(50),
+	[PhoneModel] varchar(50),
+	[PhoneNumber] int,
+
+	PRIMARY KEY([ID])
 ) 
 
-CREATE TABLE RF_ID_Tags
-(
-	RF_ID_Number int primary key,
-	RF_ID_User_ID int references ApplicationUsers(UserID)
+CREATE TABLE [Tag] (
+	[ID] int IDENTITY(1, 1),
+	[User] int,
+
+	PRIMARY KEY([ID]),
+	FOREIGN KEY([User]) REFERENCES [ApplicationUser]([ID])
 )
 
-INSERT into Restaurant VALUES (00001, 'Spur')
-INSERT into Restaurant VALUES (00002, 'Steers')
-INSERT into Restaurant VALUES (00003, 'Nandos')
+INSERT into [Restaurant] VALUES ('Spur')
+INSERT into [Restaurant] VALUES ('Steers')
+INSERT into [Restaurant] VALUES ('Nandos')
 
-INSERT into ApplicationUsers VALUES ('0705617', 'Justin','Timberlake', 'phone1', 'password', 1234567890, 00001)
-INSERT into ApplicationUsers VALUES ('0705631', 'Mariah', 'Carrey', 'phone1', 'password', 1234567890, 00002)
-INSERT into ApplicationUsers VALUES ('0705655', '2','Pac', 'phone1', 'password', 1234567890, 00001)
-INSERT into ApplicationUsers VALUES ('0705693', 'Britany','Spears', 'phone1', 'password', 1234567890, 00002)
-INSERT into ApplicationUsers VALUES ('0715605', 'Bob', 'Marley', 'phone1', 'password', 1234567890, 00003)
-INSERT into ApplicationUsers VALUES ('0725609', 'Andrea', 'Bocelli', 'phone1', 'password', 1234567890, 00001)
-INSERT into ApplicationUsers VALUES ('0745603', 'Skwatta', 'Camp', 'phone1', 'password', 1234567890, 00003)
-INSERT into ApplicationUsers VALUES ('0765607', 'BB','King', 'phone1', 'password', 1234567890, 00002)
-INSERT into ApplicationUsers VALUES ('0785601', 'Dolly' ,'Parton', 'phone1', 'password', 1234567890, 00003)
+INSERT into [ApplicationUser] VALUES ('Justin','Timberlake', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('Mariah', 'Carrey', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('2', 'Pac', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('Britany', 'Spears', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('Bob', 'Marley', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('Andrea', 'Bocelli', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('Skwatta', 'Camp', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('BB','King', 'password', 'phone1', 1234567890)
+INSERT into [ApplicationUser] VALUES ('Dolly' ,'Parton', 'password', 'phone1', 1234567890)
 
-INSERT into RF_ID_Tags VALUES (0000112, 0705617)
-INSERT into RF_ID_Tags VALUES (0000113, 0705631)
-INSERT into RF_ID_Tags VALUES (0000114, 0705655)
-INSERT into RF_ID_Tags VALUES (0000115, 0705693)
-INSERT into RF_ID_Tags VALUES (0000116, 0715605)
-INSERT into RF_ID_Tags VALUES (0000117, 0745603)
-INSERT into RF_ID_Tags VALUES (0000118, 0765607)
-INSERT into RF_ID_Tags VALUES (0000119, 0785601)
+INSERT into [Tag] VALUES (1)
+INSERT into [Tag] VALUES (2)
+INSERT into [Tag] VALUES (3)
+INSERT into [Tag] VALUES (5)
+INSERT into [Tag] VALUES (6)
+INSERT into [Tag] VALUES (7)
+INSERT into [Tag] VALUES (8)
+INSERT into [Tag] VALUES (9)
