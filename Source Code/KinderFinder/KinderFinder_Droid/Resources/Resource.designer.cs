47c79ba7bef776dc7a214b1233f42472134536be
Teshlen <tnadesan@gmail.com>
2014-07-29 23:25:12 +0200

    added view rfid tags

--- Source Code/KinderFinder/KinderFinder_Droid/Resources/Resource.designer.cs
+++ Source Code/KinderFinder/KinderFinder_Droid/Resources/Resource.designer.cs
@@ -72,23 +72,29 @@ namespace KinderFinder_Droid
 			// aapt resource value: 0x7f050004
 			public const int Main_Register = 2131034116;
 			
-			// aapt resource value: 0x7f050007
-			public const int Register_Email = 2131034119;
-			
-			// aapt resource value: 0x7f050005
-			public const int Register_FirstName = 2131034117;
-			
 			// aapt resource value: 0x7f050008
-			public const int Register_Password = 2131034120;
-			
-			// aapt resource value: 0x7f050009
-			public const int Register_PasswordConfirm = 2131034121;
-			
-			// aapt resource value: 0x7f05000a
-			public const int Register_Register = 2131034122;
+			public const int Register_Email = 2131034120;
 			
 			// aapt resource value: 0x7f050006
-			public const int Register_Surname = 2131034118;
+			public const int Register_FirstName = 2131034118;
+			
+			// aapt resource value: 0x7f050009
+			public const int Register_Password = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int Register_PasswordConfirm = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int Register_Register = 2131034123;
+			
+			// aapt resource value: 0x7f050007
+			public const int Register_Surname = 2131034119;
+			
+			// aapt resource value: 0x7f050005
+			public const int button1 = 2131034117;
+			
+			// aapt resource value: 0x7f05000c
+			public const int checkedTextView1 = 2131034124;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
@@ -111,6 +117,9 @@ namespace KinderFinder_Droid
 			
 			// aapt resource value: 0x7f030001
 			public const int Register = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int View_RFID_Tags = 2130903042;
 			
 			static Layout()
 			{
