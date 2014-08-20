bae307d7e7d5382ac403c5a92737bfbe73d40d9e
P. Chiu 11063612 <P.Chiu@tuks.co.za>
2014-08-20 18:19:36 +0200

    AdminPortal update

---
+++ Source Code/AdminPortal/AdminPortal/Controllers/ReportsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdminPortal.Models;
+
+namespace AdminPortal.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private KinderFinderEntities db = new KinderFinderEntities();
+
+        // GET: Reports
+        public ActionResult Index()
+        {
+            var tags = db.Tags.Include(t => t.Patron);
+            return View(tags.ToList());
+        }
+
+        // GET: Reports/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tag tag = db.Tags.Find(id);
+            if (tag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tag);
+        }
+
+        // GET: Reports/Create
+        public ActionResult Create()
+        {
+            ViewBag.CurrentPatron = new SelectList(db.Patrons, "ID", "FirstName");
+            return View();
+        }
+
+        // POST: Reports/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,CurrentPatron,Label")] Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tags.Add(tag);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CurrentPatron = new SelectList(db.Patrons, "ID", "FirstName", tag.CurrentPatron);
+            return View(tag);
+        }
+
+        // GET: Reports/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tag tag = db.Tags.Find(id);
+            if (tag == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CurrentPatron = new SelectList(db.Patrons, "ID", "FirstName", tag.CurrentPatron);
+            return View(tag);
+        }
+
+        // POST: Reports/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CurrentPatron,Label")] Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tag).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CurrentPatron = new SelectList(db.Patrons, "ID", "FirstName", tag.CurrentPatron);
+            return View(tag);
+        }
+
+        // GET: Reports/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tag tag = db.Tags.Find(id);
+            if (tag == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tag);
+        }
+
+        // POST: Reports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tag tag = db.Tags.Find(id);
+            db.Tags.Remove(tag);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
