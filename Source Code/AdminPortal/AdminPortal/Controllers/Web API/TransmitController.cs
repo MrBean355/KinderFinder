1fb54f8e4753265ba51e9dd8230e0eb6d57b1a5b
MrBean355 <mrbean355@gmail.com>
2014-09-22 20:20:21 +0200

    AdminPortal updates

---
+++ Source Code/AdminPortal/AdminPortal/Controllers/Web API/TransmitController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace AdminPortal.Controllers.Web_API {
+
+	public class TransmitController : ApiController {
+		
+		[HttpPost]
+		public IHttpActionResult Transmit(List<RequestDetails> details) {
+			foreach (var item in details) {
+				System.Diagnostics.Debug.WriteLine(item.id + ": " + item.distance);
+			}
+
+			return Ok();
+		}
+
+		public struct RequestDetails {
+			public string id;
+			public float distance;
+		}
+	}
+}
